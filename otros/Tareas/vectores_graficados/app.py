609611e4570d3673173282b66584bc214c775502
Chriss005 <cristianalrcon05@gmail.com>
2025-04-15 03:20:10 -0500

    proyecto calculadora de vectores, implementando objetos

--- otros/Tareas/vectores_graficados/app.py
+++ otros/Tareas/vectores_graficados/app.py
@@ -1,55 +1,55 @@
 import math
 import matplotlib.pyplot as plt
 
-def pedirComponentes():
-    X = float(input("Introduce la componente X del vector: "))
-    Y = float(input("Introduce la componente Y del vector: "))
-    return X, Y
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-def mostrarFormaVectorial(X, Y):
-    print(f"Forma vectorial: ({X}, {Y})")
+    def mostrarFormaVectorial(self):
+        print(f"Forma vectorial: ({self.x}i, {self.y}j)")
 
-def obtenerCuadrante(x, y):
-    if(x > 0 and y > 0):
-        return 1
-    elif(x < 0 and y > 0):
-        return 2
-    elif(x < 0 and y < 0):
-        return 3
-    elif(x > 0 and y < 0):
-        return 4
-    
-def calcularModulo(x, y):
-    return math.sqrt(math.pow(x, 2) + math.pow(y, 2))
+    def obtenerCuadrante(self):
+        if(self.x > 0 and self.y > 0):
+            return 1
+        elif(self.x < 0 and self.y > 0):
+            return 2
+        elif(self.x < 0 and self.y < 0):
+            return 3
+        elif(self.x > 0 and self.y < 0):
+            return 4
+        
+    def calcularModulo(self):
+        return math.sqrt(math.pow(self.x, 2) + math.pow(self.y, 2))
 
 
-def graficarVector(x, y):
-    plt.figure()
-    plt.axhline(0, color='gray', linewidth=0.5)
-    plt.axvline(0, color='gray', linewidth=0.5)
-    plt.quiver(0, 0, x, y, angles='xy', scale_units='xy', scale=1, color='blue')
-    plt.xlim(-10, 10)
-    plt.ylim(-10, 10)
-    plt.grid()
-    plt.title("Vector en el plano")
-    plt.xlabel("X")
-    plt.ylabel("Y")
-    plt.gca().set_aspect('equal', adjustable='box')
-    plt.show()
+    def graficarVector(self, cordenadaXInicio, cordenadaYInicio):
+        plt.figure()
+        plt.axhline(0, color='gray', linewidth=0.5)
+        plt.axvline(0, color='gray', linewidth=0.5)
+        plt.quiver(cordenadaXInicio, cordenadaYInicio, self.x, self.y, angles='xy', scale_units='xy', scale=1, color='blue')
+        plt.xlim(-10, 10)
+        plt.ylim(-10, 10)
+        plt.grid()
+        plt.title("Vector en el plano")
+        plt.xlabel("X")
+        plt.ylabel("Y")
+        plt.gca().set_aspect('equal', adjustable='box')
+        plt.show()
 
 def main():
 
-    X, Y = pedirComponentes()
+    vector = Vector(2, 2)    
     
-    mostrarFormaVectorial(X, Y)
+    vector.mostrarFormaVectorial()
 
-    modulo = calcularModulo(X, Y)
+    modulo = vector.calcularModulo()
     print(f"Módulo del vector: {modulo}")
 
-    cuadrante = obtenerCuadrante(X, Y)
+    cuadrante = vector.obtenerCuadrante()
     print(f"Cuadrante: {cuadrante}")
 
-    graficarVector(X, Y)
+    vector.graficarVector(0,0)
 
     input("\nPresiona Enter para salir...")
 
